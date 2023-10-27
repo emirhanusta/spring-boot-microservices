b5a958d9ad84470c928715cb307adbf805ca6fe0
emirhanusta <emirhan1usta@gmail.com>
2023-10-27 23:13:50 +0300

    Account class type converted from java to kotlin

--- account-service/src/main/java/accountservice/model/Account.kt
+++ account-service/src/main/java/accountservice/model/Account.kt
@@ -1,77 +1,16 @@
-package accountservice.model;
+package accountservice.model
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.annotation.Transient;
-import org.springframework.data.mongodb.core.mapping.Document;
-
-import java.util.UUID;
+import org.springframework.data.mongodb.core.mapping.Document
+import java.util.*
 
 @Document(collection = "accounts")
-public class Account {
+data class Account @JvmOverloads constructor(
 
-    @Transient
-    private UUID uuid = UUID.randomUUID();
-    @Id
-    private String id = uuid.toString();
-    private String username;
-    private String name;
-    private String surname;
-    private String email;
-    private String password;
-    private boolean active = true;
-    public Account(String username, String name, String surname, String email, String password) {
-        this.username = username;
-        this.name = name;
-        this.surname = surname;
-        this.email = email;
-        this.password = password;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setActive(boolean active) {
-        this.active = active;
-    }
-}
+    val id: String = UUID.randomUUID().toString(),
+    var username: String,
+    var name: String,
+    var surname: String,
+    var email: String,
+    var password: String,
+    var active: Boolean? = true
+)
